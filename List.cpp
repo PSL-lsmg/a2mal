2edaffc45398353b84ca3b596a1b9efdf33c0561
lijason103 <lijason103@gmail.com>
2017-06-08 11:53:01 -0700

    Add files via upload

--- List.cpp
+++ List.cpp
@@ -1,17 +1,19 @@
 /*
- * List.h
- *
+ * List.cpp
+ * 
  * Class Description: A value-oriented List data collection ADT.
  * Class Invariant: Data collection with the following characteristics:
  *                   - Each element is unique (no duplicates).
  *                   - It is sorted by ascending sort order of its elements.
- *                   - Its data structure is expandable: when full, it expands to accommodate
+ *                   - Its data structure is expandable: when full, it expands to accommodate 
  *                     new insertion. This is done unbeknown to the client code.
  *
  * Last modified on: May 2017
  * Author: AL
  */
 
+#pragma once
+#include <iostream>
 #include <string>
 #include "Patient.h"
 #include "List.h"
@@ -19,74 +21,77 @@
 using namespace std;
 
 
-
 // We must add at least 1 constructor and a destructor
-
+	
 // Default constructor
 List::List()
 {
-    
+	//initialize Patient arrays
+	for(int i = 0; i <= 9; i++ )
+	{
+		mainArray[i] = new Patient[initialSize];
+		elementTracker[i] = initialSize;
+	}
+
+	// Patient a;
+	// mainArray[0][0] = a;
+	// cout << mainArray[0][0].getName() << endl;
+
 }
 
-// Other constructor
-//List::List(string careCard)
-//{
-//    
-//}
+List::List(const List& lst)
+{
 
-// Destructor
+}
 
+List::~List()
+{
 
-
-// *** Start of Public Interface ***
-// Note: Public Interface must not be modified.
+}
 
 // Description: Returns the total element count currently stored in List.
 int List::getElementCount() const
 {
-    return 1;
+	return 0;
 }
 
 // Description: Insert an element.
-//              When "this" List is full, its data structure is expanded to accommodate
+//              When "this" List is full, its data structure is expanded to accommodate 
 //              a new element. This is done unbeknown to the client code.
 //              If the insertion is successful, true is returned otherwise, false is returned.
-// Precondition: newElement must not already be in data collection.
-// Postcondition: newElement inserted and the appropriate elementCount has been incremented.
+// Precondition: newElement must not already be in data collection.  
+// Postcondition: newElement inserted and the appropriate elementCount has been incremented.	
 bool List::insert(const Patient& newElement)
 {
-    return false;
+
+	return false;
 }
 
-// Description: Remove an element.
+// Description: Remove an element. 
 //              If the removal is successful, true is returned otherwise, false is returned.
-// Postcondition: toBeRemoved is removed, the appropriate elementCount has been decremented.
-bool List::remove( const Patient& toBeRemoved )
+// Postcondition: toBeRemoved is removed, the appropriate elementCount has been decremented.	
+bool List::remove( const Patient& toBeRemoved)
 {
-    return false;
+	return false;
 }
-
+	
 // Description: Remove all elements.
 void List::removeAll()
 {
-    
-}
 
+}
+   
 // Description: Search for target element and returns a pointer to it if found,
 //              otherwise, returns NULL.
 Patient* List::search(const Patient& target)
 {
-    for(int i = 0; i < elementCount; i++)
-    {
-        if(elements[i] == target)
-        {
-            return &elements[i];
-        }
-    }
+	return NULL;
 }
-
+   
 // Description: Prints all n elements stored in List in sort order and does so in O(n).
 void List::printList()
 {
-    
+
 }
+	
+// *** End of Public Interface ***
--- List.h
+++ List.h
@@ -1,11 +1,11 @@
 /*
  * List.h
- *
+ * 
  * Class Description: A value-oriented List data collection ADT.
  * Class Invariant: Data collection with the following characteristics:
  *                   - Each element is unique (no duplicates).
  *                   - It is sorted by ascending sort order of its elements.
- *                   - Its data structure is expandable: when full, it expands to accommodate
+ *                   - Its data structure is expandable: when full, it expands to accommodate 
  *                     new insertion. This is done unbeknown to the client code.
  *
  * Last modified on: May 2017
@@ -20,62 +20,52 @@ using namespace std;
 
 
 class List  {
-    
-private:
-    
-    // We must complete this section
-    static const int MAX_ELEMENTS = 9;
-    int elementCount;
-    int capacity;
-    Patient elements[MAX_ELEMENTS];
-    Patient elementTracker[0];
-    
-    
-    
-    
+
+private:	
+// We must complete this section
+	int initialSize = 3;
+	Patient** mainArray = new Patient*[10];
+	int elementTracker[10];
+
+   
 public:
-    
-    // We must add at least 1 constructor and a destructor
-    
-    // Default constructor
-    List();
-    
-    // Other constructor
-    
-    
-    // Destructor
-    
-    
-    
-    // *** Start of Public Interface ***
-    // Note: Public Interface must not be modified.
-    
-    // Description: Returns the total element count currently stored in List.
-    int  getElementCount() const;
-    
-    // Description: Insert an element.
-    //              When "this" List is full, its data structure is expanded to accommodate
+
+// We must add at least 1 constructor and a destructor
+	
+	// Default constructor
+	List();
+	List(const List& lst);
+	~List();
+
+// *** Start of Public Interface ***
+// Note: Public Interface must not be modified.
+
+	// Description: Returns the total element count currently stored in List.
+	int getElementCount() const;
+
+	// Description: Insert an element.
+    //              When "this" List is full, its data structure is expanded to accommodate 
     //              a new element. This is done unbeknown to the client code.
-    //              If the insertion is successful, true is returned otherwise, false is returned.
-    // Precondition: newElement must not already be in data collection.
-    // Postcondition: newElement inserted and the appropriate elementCount has been incremented.
-    bool insert(const Patient& newElement);
-    
-    // Description: Remove an element.
-    //              If the removal is successful, true is returned otherwise, false is returned.
-    // Postcondition: toBeRemoved is removed, the appropriate elementCount has been decremented.
-    bool remove( const Patient& toBeRemoved );
-    
-    // Description: Remove all elements.
-    void removeAll();
-    
-    // Description: Search for target element and returns a pointer to it if found,
-    //              otherwise, returns NULL.
-    Patient* search(const Patient& target);
-    
-    // Description: Prints all n elements stored in List in sort order and does so in O(n).
-    void printList();
-    
-    // *** End of Public Interface ***
-    
+	//              If the insertion is successful, true is returned otherwise, false is returned.
+	// Precondition: newElement must not already be in data collection.  
+	// Postcondition: newElement inserted and the appropriate elementCount has been incremented.	
+	bool insert(const Patient& newElement);
+
+	// Description: Remove an element. 
+	//              If the removal is successful, true is returned otherwise, false is returned.
+	// Postcondition: toBeRemoved is removed, the appropriate elementCount has been decremented.	
+	bool remove( const Patient& toBeRemoved );
+	
+	// Description: Remove all elements.
+	void removeAll();
+   
+	// Description: Search for target element and returns a pointer to it if found,
+	//              otherwise, returns NULL.
+	Patient* search(const Patient& target);
+   
+	// Description: Prints all n elements stored in List in sort order and does so in O(n).
+	void printList();
+	
+// *** End of Public Interface ***
+
 }; // end List
