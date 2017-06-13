a7af1c281ef6517a9a0946b6562451c46179e380
PSL-lsmg <priscilla.lee.international@gmail.com>
2017-06-12 17:59:45 -0700

    Create List.cpp

--- List.cpp
+++ List.cpp
@@ -8,8 +8,8 @@
  *                   - Its data structure is expandable: when full, it expands to accommodate 
  *                     new insertion. This is done unbeknown to the client code.
  *
- * Last modified on: May 2017
- * Author: AL
+ * Last modified on: June 2017
+ * Author: Jason Li, Priscilla (Fae Yein) Lee
  */
 
 #include <iostream>
